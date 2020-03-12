1feb547722d245af8e7fb8ea92d6e471eabd4720
YzolaPhilo <yzolaphilo@126.com>
2020-03-13 00:03:00 +0800

    增加ImgParam参数结构，并修改接口

--- thrift-interface/ImgService.thrift
+++ thrift-interface/ImgService.thrift
@@ -1,5 +1,5 @@
 service ImgService {
-    InvokeResult ImgStreamTransfer(1:binary imgPtr, 2:string jsonParam)
+    InvokeResult ImgStreamTransfer(1:binary imgPtr, 2:ImgParameter param)
 }
 
 enum ResponseCode {
@@ -10,4 +10,13 @@ enum ResponseCode {
 struct InvokeResult {
     1: required ResponseCode code;
     2: optional string Message;
+}
+
+struct ImgParameter {
+  1:string materialName
+  2:string floor
+  3:string sn
+  4:string slice
+  5:i32 width
+  6:i32 height
 }
