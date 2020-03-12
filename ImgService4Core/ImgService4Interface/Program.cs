2f947a562d3f19cc6cc56ee1972840bcc0fa8a61
YzolaPhilo <yzolaphilo@126.com>
2020-03-12 22:43:42 +0800

    修改头文件引用

--- ImgService4Core/ImgService4Interface/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ImgService4Interface {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
