1f704f2c73265d522c20a9dbea69358f49cca4c3
YzolaPhilo <yzolaphilo@126.com>
2020-03-12 23:43:21 +0800

    增加netcore的代码生成，完成server工程

---
+++ ImgService4Core/ImgService4Server/ImgServImpl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace ImgService4Server {
+    public class CImgServImpl : ImgService.IAsync {
+
+        Logger logger = LogManager.GetLogger("*");
+        public Task<InvokeResult> ImgStreamTransferAsync(byte[] imgPtr, string jsonParam, CancellationToken cancellationToken = default) {
+            return Task.Run(() => {
+                InvokeResult result = new InvokeResult();
+                logger.Info(imgPtr.Length + ", " + jsonParam);
+                return result;
+            });
+        }
+    }
+}
---
+++ ImgService4Core/ImgService4Server/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Windows;
+using Microsoft.Extensions.Logging.Abstractions;
+using Thrift.Protocols;
+using Thrift.Server;
+using Thrift.Transports;
+using Thrift.Transports.Server;
+
+namespace ImgService4Server {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        
+        public MainWindow() {
+            InitializeComponent();
+            //直接启动服务程序
+            CancellationToken token = new CancellationToken();
+            TServerTransport serverTransport = new TServerSocketTransport(31280);
+
+            TBinaryProtocol.Factory _binFactory = new TBinaryProtocol.Factory();
+            TBinaryProtocol.Factory _binFactory_output = new TBinaryProtocol.Factory();
+
+            ImgService.AsyncProcessor Processor = new ImgService.AsyncProcessor(new CImgServImpl());
+
+            Thrift.Server.TBaseServer server = new AsyncBaseServer(Processor, serverTransport, _binFactory, _binFactory_output, new NullLoggerFactory());
+            server.ServeAsync(token);
+        }
+    }
+}
